6c7830ad5e1fe237f8083c1d70c2b279e6ee4115
chaya <chayaz.haor@gmail.com>
2022-02-08 09:22:56 -0500

    Fixed quad equation based on feedback

--- src/main/java/HW1/QuadEquation.java
+++ src/main/java/HW1/QuadEquation.java
@@ -1,18 +1,9 @@
 package HW1;
 
 public class QuadEquation {
-    int a;
-    int b;
-    int c;
-
-    public double[] findResults() {
-        double[] results = new double[2];
-        results[0] = -b + Math.sqrt(b * b - (4 * a * c));
-        results[1] = -b - Math.sqrt(b * b - (4 * a * c));
-
-        return results;
-    }
-
+    private final int a;
+    private final int b;
+    private final int c;
 
     public QuadEquation(int a, int b, int c) {
         this.a = a;
@@ -20,8 +11,18 @@ public class QuadEquation {
         this.c = c;
     }
 
+    public double[] findResults() {
+        double[] results = new double[2];
+        results[0] = (-b + Math.sqrt(b * b - (4 * a * c))) / (2 * a);
+        results[1] = (-b - Math.sqrt(b * b - (4 * a * c))) / (2 * a);
+
+        return results;
+    }
+
+
     @Override
     public String toString() {
-        return "The possible results are " + findResults()[0] + " and " + findResults()[1]                    ;
+        double[] results=findResults();
+        return "The possible results are " + results[0] + " and " + results[1];
     }
 }
