888c2eafbba395070f68bcb7d91f65618746bbd0
chaya <chayaz.haor@gmail.com>
2022-04-29 16:03:44 -0400

    working on test

--- src/test/java/weather/GetCurrentWeatherTest.java
+++ src/test/java/weather/GetCurrentWeatherTest.java
@@ -1,22 +1,31 @@
 package weather;
 
+import io.reactivex.Observable;
 import org.junit.jupiter.api.Test;
+import weather.json.CurrentWeather;
 
-import java.io.IOException;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GetCurrentWeatherTest {
 
-
     @Test
-    void getWeather() throws IOException {
+    void getWeather() {
         //given
         GetCurrentWeather getCurrentWeather = new GetCurrentWeather();
+        String zip = "10019";
+        Observable<CurrentWeather> observable = getCurrentWeather.getCurrentWeather(zip);
+
         //when
-        String zip="10019";
+        double expectedTemp = observable.blockingFirst().getTemperature();
+        double maxTemp = observable.blockingFirst().getMaxTemperature();
+        double minTemp = observable.blockingFirst().getMinTemperature();
 
         //then
+        assertTrue(expectedTemp>0);
+        assertTrue(maxTemp>0);
+        assertTrue(minTemp>0);
+
+
 
     }
 
